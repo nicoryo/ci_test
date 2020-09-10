5f98b34afd56c3d368fd05ee0601fe801f50307f
nicoryo <nicoryo.giogio@gmail.com>
2020-09-10 17:11:45 +0900

    change spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe User, type: :model do
   describe "バリデーション" do
     it "nameの値が空の場合、エラーを返すこと" do
       user = User.new(name: "", email: "test@example.com")
-      expect(user.valid?).to eq(false)
+      expect(user.valid?).to eq(true)
     end
 
     it "emailの値が空の場合、エラーを返すこと" do
       user = User.new(name: "TestUser", email: "")
-      expect(user.valid?).to eq(false)
+      expect(user.valid?).to eq(true)
     end
   end
 end
